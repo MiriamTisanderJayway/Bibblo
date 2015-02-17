95ad7cd96a879b883446995fbad966cba350a60c
Miriam Tisander <miriam.tisander@jayway.com>
2015-02-17 17:11:13 +0100

    Initial Commit

---
+++ Bibblo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bibblo
+//
+//  Created by Jayway on 2015-02-17.
+//  Copyright (c) 2015 Jayway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
